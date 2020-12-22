2d825b2ab921f6ad84e2f76607d15e721feef030
Maria Victória Uzum <mvic.uzum@gmail.com>
2020-12-22 13:04:18 -0300

    Criação da API com Spring Boot

---
+++ TestePraticoXbrain/src/main/java/com/xbrain/teste/TestePraticoXbrainApplication.java
@@ -0,0 +1,13 @@
+package com.xbrain.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePraticoXbrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePraticoXbrainApplication.class, args);
+	}
+
+}
---
+++ TestePraticoXbrain/src/test/java/com/xbrain/teste/TestePraticoXbrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbrain.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePraticoXbrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
