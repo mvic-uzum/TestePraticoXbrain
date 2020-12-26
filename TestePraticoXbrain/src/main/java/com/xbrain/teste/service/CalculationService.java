85bde220ab183ad45dd31552b9e8b01c3371111e
Maria Victória Uzum <mvic.uzum@gmail.com>
2020-12-26 20:12:36 -0300

    Função soma funcionando

--- TestePraticoXbrain/src/main/java/com/xbrain/teste/service/CalculationService.java
+++ TestePraticoXbrain/src/main/java/com/xbrain/teste/service/CalculationService.java
@@ -1,18 +1,15 @@
 package com.xbrain.teste.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.xbrain.teste.model.Produto;
 
 //Calcula o valor total do pedido
 public class CalculationService{
 	
-	public double somaTotal(List<Produto> produto){
-		double valor_total = 0;
-		for(int i=0;i<produto.size();i++){
-			valor_total += produto.get(i).getValor();
-		}
-		return valor_total;
+	public double somaTotal(List<Produto> produtos){
+		return produtos.stream().map(Produto::getValor).collect(Collectors.summingDouble(Double::doubleValue));
 	}
 		
 }
